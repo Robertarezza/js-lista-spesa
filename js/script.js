028515ff435976abca7951755a93d885c05634bf
Robertarezza <roberta.rezza@gmail.com>
2024-04-03 19:31:09 +0200

    fix: while cicle

--- js/script.js
+++ js/script.js
@@ -4,30 +4,46 @@
 
 
 
-const shoppingList = ["latte", "pasta", "formaggio", "biscotti","pane","marmellata"]
-let listElem = document.querySelector(".shopping-list");
+ const shoppingList = ["latte", "pasta", "formaggio", "biscotti","pane","marmellata"]
+ let listElem = document.querySelector(".shopping-list");
 
 
-let i = 0;
-//let result = "";
-while (i < shoppingList.length){
-    const curItem = shoppingList[i];
-    console.log(curItem);
+ let i = 0;
+// let result = "";
+ while (i < shoppingList.length){
+     const curItem = shoppingList[i];
+     console.log(curItem);
     i++
 
-    //result += `<li>${curItem}</li>`;
+//     result += `<li>${curItem}</li>`;
 
-    //oppure si può stampare cosi nel dom
-    const listItemElem = document.createElement("li"); //crei l'elemento "tag"
-    listItemElem.innerHTML = curItem; //lo scrivi
-    listElem.append(listItemElem); //aggiungi tutto nel listItemElem
+//     //oppure si può stampare cosi nel dom
+   const listItemElem = document.createElement("li"); //crei l'elemento "tag"
+  listItemElem.innerHTML = curItem; //lo scrivi
+   listElem.append(listItemElem); //aggiungi tutto nel listItemElem
    
-    listItemElem.addEventListener("click", function () {
-        listItemElem.style.color = "yellow";
-        listItemElem.style.textDecoration = "line-through";
+listItemElem.addEventListener("click", function () {
+listItemElem.style.color = "yellow";
+listItemElem.style.textDecoration = "line-through";
         
         
-    })
-}
-    //listElem.innerHTML = result;
+})
+ }
+//     listElem.innerHTML = result;
 
+
+    // const listaSpesa = ["pane","latte","biscotti","marmellata"];
+    // let elemList = document.querySelector(".shopping-list");
+
+
+    // let i = 0;
+    // let pezzo = "";
+    // while (i < listaSpesa.length){
+    //     pezzoPerPezzo = listaSpesa[i];
+    //     console.log(pezzoPerPezzo);
+    //     i++
+
+    //     pezzo += `<li>${pezzoPerPezzo}</li>`;
+    // }
+
+    // elemList.innerHTML = pezzo;
